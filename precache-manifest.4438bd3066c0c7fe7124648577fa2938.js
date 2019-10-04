b74b80e39b679b996bf961cbb7983b2e4b9ea897
furkan <furkandoganktf@gmail.com>
2019-10-05 02:41:06 +0300

    Updates

---
+++ precache-manifest.4438bd3066c0c7fe7124648577fa2938.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f068d8a0046b645b51568c2618783de6",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "10e6e323277d7724be8e",
+    "url": "/resume/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "81876330289a55dbc739",
+    "url": "/resume/static/js/2.393e0e58.chunk.js"
+  },
+  {
+    "revision": "10e6e323277d7724be8e",
+    "url": "/resume/static/js/main.8de47bbb.chunk.js"
+  },
+  {
+    "revision": "699f89d957187d0ac949",
+    "url": "/resume/static/js/runtime~main.30522d0f.js"
+  }
+]);
