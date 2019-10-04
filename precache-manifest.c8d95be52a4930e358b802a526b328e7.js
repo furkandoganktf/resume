4a452cdbc17bccee601af5180d0c658b6c5aa55c
furkan <furkandoganktf@gmail.com>
2019-10-05 02:33:57 +0300

    Updates

---
+++ precache-manifest.c8d95be52a4930e358b802a526b328e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81fa98c34f25af49345010e9b5656ae7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1845de07df300e0ab273",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "81876330289a55dbc739",
+    "url": "/static/js/2.393e0e58.chunk.js"
+  },
+  {
+    "revision": "1845de07df300e0ab273",
+    "url": "/static/js/main.d06a3557.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
