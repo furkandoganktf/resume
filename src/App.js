e10c65d2e8aa2899a6dc94b85758281938e9bf79
furkan <furkandoganktf@gmail.com>
2020-01-24 23:38:20 +0300

    change basename

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
 
   render() {
     return (
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <Switch>
           <Route exact path="/" render={props => <Homepage {...props} />} />
           <Route path="/reading" render={props => <Readinglist {...props} />} />
