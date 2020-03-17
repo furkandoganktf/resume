65f2e3c480b1b790ac7a1003f023170c32b581aa
furkan <furkandoganktf@gmail.com>
2020-03-17 19:09:34 +0300

    fix 404 error

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Homepage from "./Components/Homepage.js";
 import Aboutme from "./Components/Aboutme.js";
 
 import Readinglist from "./Components/ReadingList/ReadingList";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 
 class App extends Component {
   constructor(props) {
@@ -17,11 +17,12 @@ class App extends Component {
 
   render() {
     return (
-      <Router basename={process.env.PUBLIC_URL}>
+      <Router basename={process.env.PUBLIC_URL + "/"}>
         <Switch>
           <Route exact path="/" render={props => <Homepage {...props} />} />
           <Route path="/reading" render={props => <Readinglist {...props} />} />
           <Route path="/me" render={props => <Aboutme {...props} />} />
+          <Redirect from="*" to="/" />
         </Switch>
       </Router>
     );
